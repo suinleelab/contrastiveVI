c40cf223505c3e5795610c7ce5c9e2f52f78867e
Ethan Weinberger <ewein@cs.washington.edu>
2021-10-18 17:38:20 -0700

    Add scVI model

--- contrastive_vi/__init__.py
+++ contrastive_vi/__init__.py
@@ -1,1 +1,36 @@
-"""Contrastive variational inference for single-cell expression data."""
+"""contrastive_vi setup file. copied from the scvi-tools-skeleton repo."""
+
+import logging
+
+from rich.console import Console
+from rich.logging import RichHandler
+from scvi.data import setup_anndata
+
+from ._scvi_model import SCVIModel, SCVIModule
+
+# https://github.com/python-poetry/poetry/pull/2366#issuecomment-652418094
+# https://github.com/python-poetry/poetry/issues/144#issuecomment-623927302
+try:
+    import importlib.metadata as importlib_metadata
+except ModuleNotFoundError:
+    import importlib_metadata
+
+package_name = "contrastive_vi"
+__version__ = importlib_metadata.version(package_name)
+
+logger = logging.getLogger(__name__)
+# set the logging level
+logger.setLevel(logging.INFO)
+
+# nice logging outputs
+console = Console(force_terminal=True)
+console.is_jupyter = False
+ch = RichHandler(show_path=False, console=console, show_time=False)
+formatter = logging.Formatter("mypackage: %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+# this prevents double outputs
+logger.propagate = False
+
+__all__ = ["setup_anndata", "SCVIModel", "SCVIModule"]
