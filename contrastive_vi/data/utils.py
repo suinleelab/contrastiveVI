Bugfix for log library size calculation

contrastive_vi/data/utils.py
@@ -154,7 +154,8 @@ def get_library_log_means_and_vars(adata: AnnData) -> Tuple[np.ndarray, np.ndarr
             library = count_data[adata.obs["batch"] == batch].sum(1)
         else:
             library = count_data.sum(1)
-        library_log = np.log(library)
+        library_log = np.ma.log(library)
+        library_log = library_log.filled(0.0)  # Fill invalid log values with zeros.
         library_log_means.append(library_log.mean())
         library_log_vars.append(library_log.var())
     library_log_means = np.array(library_log_means)[np.newaxis, :]